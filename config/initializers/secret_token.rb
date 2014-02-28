76811af9011bd039bda769ffcc7b49e528840106
Szuturon <ronnyszutu@gmail.com>
2014-02-28 14:21:15 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DBGStatsKeeper::Application.config.secret_token = '84807185c32755d87a540b0f543fe467510b90c252b33c792a26ddd3d4d3c026e8a14b6f3996ea0e52803b73de3782d4e94502d9ab4df2262f2c11cccc049a9e'
